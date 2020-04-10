cbc956cb0f74c779808d9e9f6c96a594be4cc5a7
qoosky <qoosky@users.noreply.github.com>
2020-04-10 19:57:29 +0900

    raise Error when library not found

--- odepy/common.py
+++ odepy/common.py
@@ -18,22 +18,24 @@ from ctypes import c_char
 from ctypes import c_uint32
 from ctypes import c_int32
 
+class OdePyError(Exception):
+    pass
+
 def __GetOdeLib():
-    odeLibName = find_library('ode')
-    if odeLibName is not None:
-        odeLib = CDLL(odeLibName, use_errno=True)
+    if find_library('ode') is not None:
+        return CDLL(find_library('ode'), use_errno=True)
+    ldLibraryPath = environ.get('LD_LIBRARY_PATH')
+    if ldLibraryPath is None:
+        ldLibraryPath = []
     else:
-        ldLibraryPath = environ.get('LD_LIBRARY_PATH')
-        if ldLibraryPath is None:
-            ldLibraryPath = []
-        else:
-            ldLibraryPath = ldLibraryPath.split(pathsep)
-        localOdeInstallLibDir = path.join(environ.get('HOME'), '.odepy', 'lib')
-        ldLibraryPath.append(localOdeInstallLibDir)
-        environ['LD_LIBRARY_PATH'] = pathsep.join(ldLibraryPath)
-        environ['LIBRARY_PATH'] = pathsep.join(ldLibraryPath)
-        odeLib = CDLL(path.join(localOdeInstallLibDir, find_library('ode')), use_errno=True)
-    return odeLib
+        ldLibraryPath = ldLibraryPath.split(pathsep)
+    localOdeInstallLibDir = path.join(environ.get('HOME'), '.odepy', 'lib')
+    ldLibraryPath.append(localOdeInstallLibDir)
+    environ['LD_LIBRARY_PATH'] = pathsep.join(ldLibraryPath)
+    environ['LIBRARY_PATH'] = pathsep.join(ldLibraryPath)
+    if find_library('ode') is None:
+        raise OdePyError('ODE library not found.')
+    return CDLL(path.join(localOdeInstallLibDir, find_library('ode')), use_errno=True)
 
 def __load(lib, name, restype, *args):
     return (CFUNCTYPE(restype, *args))((name, lib))
