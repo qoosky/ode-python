dff3ae2d0aa8b44a12d23d5fc9edd5468dc083ef
qoosky <qoosky@users.noreply.github.com>
2020-04-10 21:11:14 +0900

    finish collision_space.py

---
+++ odepy/collision_space.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+from .common import loadOde
+from .common import dGeomID
+from .common import dSpaceID
+from .common import dVector3
+
+from ctypes import c_void_p
+from ctypes import c_int32
+
+dNearCallback = loadOde('dNearCallback', None, c_void_p, dGeomID, dGeomID)
+dSimpleSpaceCreate = loadOde('dSimpleSpaceCreate', dSpaceID, dSpaceID)
+dHashSpaceCreate = loadOde('dHashSpaceCreate', dSpaceID, dSpaceID)
+dQuadTreeSpaceCreate = loadOde('dQuadTreeSpaceCreate', dSpaceID, dSpaceID, dVector3, dVector3, c_int32)
+dSweepAndPruneSpaceCreate = loadOde('dSweepAndPruneSpaceCreate', dSpaceID, dSpaceID, c_int32)
+dSpaceDestroy = loadOde('dSpaceDestroy', None, dSpaceID)
+dHashSpaceSetLevels = loadOde('dHashSpaceSetLevels', None, dSpaceID, c_int32, c_int32)
+dHashSpaceGetLevels = loadOde('dHashSpaceGetLevels', None, dSpaceID, POINTER(c_int32), POINTER(c_int32))
+dSpaceSetCleanup = loadOde('dSpaceSetCleanup', None, dSpaceID, c_int32)
+dSpaceGetCleanup = loadOde('dSpaceGetCleanup', c_int32, dSpaceID)
+dSpaceSetSublevel = loadOde('dSpaceSetSublevel', None, dSpaceID, c_int32)
+dSpaceGetSublevel = loadOde('dSpaceGetSublevel', c_int32, dSpaceID)
+dSpaceSetManualCleanup = loadOde('dSpaceSetManualCleanup', None, dSpaceID, c_int32)
+dSpaceGetManualCleanup = loadOde('dSpaceGetManualCleanup', c_int32, dSpaceID)
+dSpaceAdd = loadOde('dSpaceAdd', None, dSpaceID, dGeomID)
+dSpaceRemove = loadOde('dSpaceRemove', None, dSpaceID, dGeomID)
+dSpaceQuery = loadOde('dSpaceQuery', c_int32, dSpaceID, dGeomID)
+dSpaceClean = loadOde('dSpaceClean', None, dSpaceID)
+dSpaceGetNumGeoms = loadOde('dSpaceGetNumGeoms', c_int32, dSpaceID)
+dSpaceGetGeom = loadOde('dSpaceGetGeom', dGeomID, dSpaceID, c_int32)
+dSpaceGetClass = loadOde('dSpaceGetClass', c_int32, dSpaceID)
---
+++ odepy/contact.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+from .common import loadOde
+from .common import dVector3
+from .common import dReal
+from .common import dGeomID
+
+from ctypes import Structure
+from ctypes import c_int32
+
+class dContactGeom(Structure):
+
+    _fields_ = [('pos', dVector3),
+                ('normal', dVector3),
+                ('depth', dReal),
+                ('g1', dGeomID),
+                ('g2', dGeomID),
+                ('side1', c_int32),
+                ('side2', c_int32)]
+
+    def _init_(self, pos, normal, depth, g1, g2, side1, side2):
+        self.pos = pos
+        self.normal = normal
+        self.depth = depth
+        self.g1 = g1
+        self.g2 = g2
+        self.side1 = side1
+        self.side2 = side2
