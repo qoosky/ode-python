7effda212e3070a2cab62cf8246a0e86f4207f5b
qoosky <qoosky@users.noreply.github.com>
2020-04-11 20:15:39 +0900

    update test_space.py

--- test/test_space.py
+++ test/test_space.py
@@ -1,56 +1,65 @@
 # -*- coding: utf-8 -*-
 
 from pytest import fixture
+
 from ctypes import byref
 from ctypes import addressof
 
-from odepy import dNearCallback
 from odepy import dWorldStep
-from odepy import dBodySetPosition
-from odepy import dBodyGetPosition
 from odepy import dSpaceCollide
-from odepy import dBodyCreate
+from odepy import dNearCallback
+
 from odepy import dMass
 from odepy import dMassSetZero
 from odepy import dMassSetSphereTotal
+
+from odepy import dBodyCreate
 from odepy import dBodySetMass
+from odepy import dBodySetPosition
+from odepy import dBodyGetPosition
+
 from odepy import dCreateSphere
 from odepy import dGeomSetBody
 
 class NearCallback(object):
 
     def __init__(self, ground):
+        self.__count = 0
         self.__ground = ground
 
+    def GetCount(self):
+        return self.__count
+
     def Callback(self, data, o1, o2):
-        print(addressof(self.__ground.contents) == addressof(o1.contents))
-        print(addressof(self.__ground.contents) == addressof(o2.contents))
+        self.__count += 1
+        o1IsGround = addressof(self.__ground.contents) == addressof(o1.contents)
+        o2IsGround = addressof(self.__ground.contents) == addressof(o2.contents)
+        assert(o1IsGround or o2IsGround)
 
 class TestSpace(object):
 
     @fixture
     def ball(self, world, space):
-        body = dBodyCreate(world)
+        m = 1.0
+        r = 0.1
         mass = dMass()
         dMassSetZero(byref(mass))
-        m = 1.0
-        r = 0.2
         dMassSetSphereTotal(byref(mass), m, r)
+        body = dBodyCreate(world)
         dBodySetMass(body, byref(mass))
-        x0 = 0.0
-        y0 = 0.0
-        z0 = 2.0
-        dBodySetPosition(body, x0, y0, z0)
         geom = dCreateSphere(space, r)
         dGeomSetBody(geom, body)
         ball = {'body': body, 'geom': geom}
         return ball
 
     def test_collision(self, world, space, ground, ball):
-        for i in range(62):
-            nearCallback = NearCallback(ground=ground)
+        nearCallback = NearCallback(ground=ground)
+        tDelta = 0.01
+        x0 = 0.0
+        y0 = 0.0
+        z0 = 3.0
+        dBodySetPosition(ball['body'], x0, y0, z0)
+        for i in range(99):
             dSpaceCollide(space, 0, dNearCallback(nearCallback.Callback))
-            pos = dBodyGetPosition(ball['body'])
-            aaa = [pos[i] for i in range(4)]
-            print(aaa[2])
-            dWorldStep(world, 0.01)
+            assert(dWorldStep(world, tDelta) == 1)
+        assert(nearCallback.GetCount() > 0)
