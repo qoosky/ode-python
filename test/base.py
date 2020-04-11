16abef39281dcdc5cffca023568a2cfc28706fe1
qoosky <qoosky@users.noreply.github.com>
2020-04-11 16:37:00 +0900

    add test_space.py

---
+++ test/base.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+from pytest import fixture
+
+from ctypes import byref
+
+from odepy import dInitODE
+from odepy import dCloseODE
+from odepy import dWorldCreate
+from odepy import dWorldSetGravity
+from odepy import dWorldDestroy
+from odepy import dMass
+from odepy import dMassSetZero
+from odepy import dMassSetSphereTotal
+from odepy import dBodyCreate
+from odepy import dBodySetMass
+
+class TestBase(object):
+
+    @fixture
+    def g(self):
+        return 9.80665
+
+    @fixture
+    def world(self, g):
+        dInitODE()
+        world = dWorldCreate()
+        dWorldSetGravity(world, 0, 0, -g)
+        yield world
+        dWorldDestroy(world)
+        dCloseODE()
+
+    @fixture
+    def sphere(self, world):
+        r = 0.1
+        m = 1.0
+        sphere = dBodyCreate(world)
+        mass = dMass()
+        dMassSetZero(byref(mass))
+        dMassSetSphereTotal(byref(mass), m, r)
+        dBodySetMass(sphere, byref(mass))
+        return sphere
---
+++ test/test_space.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from .base import TestBase
+
+from odepy import dNearCallback
+
+class TestSpace(TestBase):
+
+    def test_space(self):
+        def mycallback(a, b, c):
+            pass
+        dNearCallback(mycallback)
+        assert True
